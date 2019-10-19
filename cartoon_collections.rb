b7009868fa332e253d5b3c77d89008378f2573fe
Wendy Harris <wendyv510@hotmail.com>
2019-10-19 14:22:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,9 +12,16 @@ def summon_captain_planet(planeteer_calls)
     puts "#{calls}!" 
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(short_words, assorted_words)
+  short_words = ["puff", "go", "two"]
+  assorted_words = ["two", "go", "industrious", "bop"]
+  long_planeteer_calls.any? >5.length = false 
 end
+
+long_planeteer_calls(short_words)
+
+long_planeteer_calls(assorted_words)
+
 
 def find_the_cheese# code an argument here
   # the array below is here to help
